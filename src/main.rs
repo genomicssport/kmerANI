4ac3774f322a434de1a202e562a9e064bb312d79
Gaurav Sablok <gsablok@ibch.poznan.pl>
2025-09-22 15:18:00 +0200

    genomicssport
    submitted application and now time to code this in the evening.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
-}
+// The prelude import enables methods we use below, specifically
+// Rng::random, Rng::sample, SliceRandom::shuffle and IndexedRandom::choose.
+use rand::prelude::*;
+
+// Get an RNG:
+let mut rng = rand::rng();
+
+// Try printing a random unicode code point (probably a bad idea)!
+println!("char: '{}'", rng.random::<char>());
+// Try printing a random alphanumeric value instead!
+println!("alpha: '{}'", rng.sample(rand::distr::Alphanumeric) as char);
+
+// Generate and shuffle a sequence:
+let mut nums: Vec<i32> = (1..100).collect();
+nums.shuffle(&mut rng);
+// And take a random pick (yes, we didn't need to shuffle first!):
+let _ = nums.choose(&mut rng);
